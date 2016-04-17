89f94ce7570950a94593b4ece7cfe1eaa5ee24ad
Marcin Miotk <mami@ailleron.com>
2016-04-17 17:04:12 +0200

    MaximumSubarray - solution - but I got Timeout while performance tests. Performance test should take max 4 sec but it takes 40 sec.

---
+++ Algorithms/MaximumSubarray/src/main/java/com/snmill/cp/Solution.java
@@ -0,0 +1,143 @@
+package com.snmill.cp;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tests = scanner.nextInt();
+        for (int t = 0; t < tests; t++) {
+            int n = scanner.nextInt();
+            int[] input = new int[n];
+            for (int ni = 0; ni < n; ni++) {
+                input[ni] = scanner.nextInt();
+            }
+            FindMaxSum find = new FindMaxSum(input);
+            System.out.println(find.findMaxSums().toString());
+        }
+    }
+
+    static class Result {
+
+        int maxContiguousSum;
+        int maxNoncontiguousSum;
+
+        public Result() {
+        }
+
+        public Result(int maxContiguousSum, int maxNoncontiguousSum) {
+            this.maxContiguousSum = maxContiguousSum;
+            this.maxNoncontiguousSum = maxNoncontiguousSum;
+        }
+
+        @Override
+        public String toString() {
+            return maxContiguousSum + " " + maxNoncontiguousSum;
+        }
+
+    }
+
+    static class FindMaxSum {
+
+        private final int[] input;
+
+        public FindMaxSum(int[] input) {
+            this.input = input;
+        }
+
+        int findMaxContiguousSum() {
+
+            if (input.length == 1) {
+                return input[0];
+            }
+
+            int[] prefixes = PrefixSums.sums(input);
+            Place place = new Place(0, input.length - 1);
+            int sum = PrefixSums.countTotal(prefixes, place.xIndex, place.yIndex);
+            while (place.hasNextSlicesToCheck()) {
+                int candidateSum = PrefixSums.countTotal(prefixes, place.xIndex, place.yIndex);
+                if (candidateSum > sum) {
+                    sum = candidateSum;
+                }
+                place.nextSlice();
+            }
+            return sum;
+        }
+
+        static class Place {
+
+            final int x;
+            final int y;
+            int xIndex;
+            int yIndex;
+
+            public Place(int x, int y) {
+                this.x = x;
+                this.y = y;
+                xIndex = x;
+                yIndex = y;
+            }
+
+            boolean hasNextSlicesToCheck() {
+                if (xIndex <= y) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            void nextSlice() {
+                yIndex--;
+                if (yIndex < xIndex) {
+                    xIndex++;
+                    yIndex = y;
+                }
+            }
+        }
+
+        int findMaxNonContiguousSum() {
+            int sum = 0;
+            int maxValue = Integer.MIN_VALUE;
+            for (int x : input) {
+                if (x > maxValue) {
+                    maxValue = x;
+                }
+                if (x > 0) {
+                    sum += x;
+                }
+            }
+            if (maxValue <= 0) {
+                return maxValue;
+            } else {
+                return sum;
+            }
+        }
+
+        Result findMaxSums() {
+            Result result = new Result();
+            result.maxNoncontiguousSum = findMaxNonContiguousSum();
+            result.maxContiguousSum = findMaxContiguousSum();
+            return result;
+        }
+    }
+
+    static class PrefixSums {
+
+        static int[] sums(int[] input) {
+            int[] prefixes = new int[input.length + 1];
+            prefixes[0] = 0;
+            for (int i = 1; i < prefixes.length; i++) {
+                prefixes[i] = prefixes[i - 1] + input[i - 1];
+            }
+            return prefixes;
+        }
+
+        static int countTotal(int[] prefixSums, int x, int y) {
+            return prefixSums[y + 1] - prefixSums[x];
+        }
+    }
+}
---
+++ Algorithms/MaximumSubarray/src/test/java/com/snmill/cp/SolutionTest.java
@@ -0,0 +1,146 @@
+package com.snmill.cp;
+
+import com.snmill.cp.Solution.FindMaxSum;
+import com.snmill.cp.Solution.PrefixSums;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class SolutionTest {
+
+    public SolutionTest() {
+    }
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    @Test
+    public void official_test() {
+        String input = ""
+                + "2 \n"
+                + "4 \n"
+                + "1 2 3 4\n"
+                + "6\n"
+                + "2 -1 2 3 4 -5";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Solution.main(null);
+        assertEquals("10 10\r\n"
+                + "10 11", outContent.toString().trim());
+    }
+
+    @Test
+    public void official_test2() {
+        String input = ""
+                + "6\n"
+                + "1\n"
+                + "1\n"
+                + "6\n"
+                + "-1 -2 -3 -4 -5 -6\n"
+                + "2\n"
+                + "1 -2\n"
+                + "3\n"
+                + "1 2 3\n"
+                + "1\n"
+                + "-10\n"
+                + "6\n"
+                + "1 -1 -1 -1 -1 5";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Solution.main(null);
+        assertEquals("1 1\r\n"
+                + "-1 -1\r\n"
+                + "1 1\r\n"
+                + "6 6\r\n"
+                + "-10 -10\r\n"
+                + "5 6", outContent.toString().trim());
+    }
+
+    @Test
+    public void findMaxNonContiguousSum_input_1() {
+        FindMaxSum solution = new FindMaxSum(new int[]{-10});
+        int maxContiguousSum = solution.findMaxNonContiguousSum();
+        assertEquals(-10, maxContiguousSum);
+    }
+
+    @Test
+    public void findMaxNonContiguousSum_input_2() {
+        FindMaxSum solution = new FindMaxSum(new int[]{-1, -2, -3, -4, -5, -6});
+        int maxContiguousSum = solution.findMaxNonContiguousSum();
+        assertEquals(-1, maxContiguousSum);
+    }
+
+    @Test
+    public void findMaxContiguousSum_input1() {
+        FindMaxSum solution = new FindMaxSum(new int[]{2, -1, 2, 3, 4, -5});
+        int maxContiguousSum = solution.findMaxContiguousSum();
+        assertEquals(10, maxContiguousSum);
+    }
+
+    @Test
+    public void prefixSums_1() {
+        int[] input = new int[]{1, -3, 7, 2, 3};
+        int[] prefixes = PrefixSums.sums(input);
+        assertEquals(input.length + 1, prefixes.length);
+        int[] expectedPrefixes = new int[]{0, 1, -2, 5, 7, 10};
+        assertArrayEquals(expectedPrefixes, prefixes);
+    }
+
+    @Test
+    public void findMaxContiguousSum_input2() {
+        FindMaxSum solution = new FindMaxSum(new int[]{1, -1, -1, -1, -1, 5});
+        int maxContiguousSum = solution.findMaxContiguousSum();
+        assertEquals(5, maxContiguousSum);
+    }
+
+    @Test
+    public void findMaxContiguousSum_input3() {
+        FindMaxSum solution = new FindMaxSum(new int[]{-1, -2, -3, -4, -5, -6});
+        int maxContiguousSum = solution.findMaxContiguousSum();
+        assertEquals(-1, maxContiguousSum);
+    }
+
+    @Test
+    public void tenPerformanceTests() throws IOException {
+        InputStream in = this.getClass().getResourceAsStream("/performance");
+        System.setIn(in);
+
+        Solution.main(null);
+        assertEquals("2617065 172083036\r\n"
+                + "1274115 193037987\r\n"
+                + "2202862 163398048\r\n"
+                + "2454939 240462364\r\n"
+                + "3239908 186256172\r\n"
+                + "2486039 202399661\r\n"
+                + "1092777 137409985\r\n"
+                + "962621 135978139\r\n"
+                + "3020911 224370860\r\n"
+                + "1755033 158953999", outContent.toString().trim());
+    }
+}
